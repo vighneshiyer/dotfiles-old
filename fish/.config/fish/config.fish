8617a60210ce3574f1aeabb8c68ca69ac2fc4776
Vighnesh Iyer <vighnesh.iyer@berkeley.edu>
2021-01-29 16:09:04 -0800

    update

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -77,6 +77,10 @@ alias office 'silent libreoffice'
 abbr -a cpr rsync -ah --progress
 abbr -a yt-audio "youtube-dl -f \"bestaudio\" -o \" %(title)s.%(ext)s\" --user-agent \"Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)\""
 abbr -a yt-video "youtube-dl -f \"bestvideo+bestaudio\" -o \" %(title)s.%(ext)s\" --user-agent \"Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)\""
+function borgstatus
+    echo (ls -lt *.log | head -n 1 | cut -d ' ' -f 10)
+    grep 'status, ' (ls -lt *.log | head -n 1 | cut -d ' ' -f 10)
+end
 
 function fix_perms
     find . -type f -exec chmod 644 '{}' \;
